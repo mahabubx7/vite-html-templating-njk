55ef6a19e082598fc751ac7abf8e140b68f13a3c
Mahabub Alam <mahabubx7@gmail.com>
2023-07-08 23:50:09 +0600

    final(staged): added-sass-scss-supports-with-tailwind-autoload

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
 import vituum from "vituum";
 import nunjucks from "@vituum/vite-plugin-nunjucks";
+// import sass from 'sass'
 
 export default defineConfig({
   plugins: [
     vituum(),
+    // sass(),
     nunjucks({
       root: "./src",
     }),
+    // sass()
   ],
   server: {
     port: 3000,
